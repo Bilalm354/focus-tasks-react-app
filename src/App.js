70d829713ae4bcd9f94426d75feb38802001af88
Bilal <bilalm354@gmail.com>
2019-11-23 15:30:17 +0000

    styling

--- src/App.js
+++ src/App.js
@@ -30,11 +30,11 @@ class App extends Component {
     return (
       <div className="App">
         <div className="container">
-          <header className="App-header">
+          {/* <header className="App-header">
             <div className="App-title">
               <h1>Focus Tasks</h1>
             </div>
-          </header>
+          </header> */}
           <div className = "Instructions">
             {/* <p>Enter all the tasks you have to complete. Then click start work timer. Complete your tasks in order and mark them as complete. Then move on to the next one. You will work in 25 minute intervals. The timer will tell you to take a short break (5 mins) after every interval of work and a long break (15 mins) after every 4th inerval. </p> */}
           </div>
